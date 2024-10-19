ee9dc4c21111f087d54a480d7624e9c1a0ce410e
LOstDev404 <127157866+LOstDev404@users.noreply.github.com>
2024-10-19 00:23:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,5 +118,5 @@ if main_option == 'Random Item Skyblock':
             os.remove(output_file)
 
 if main_option == '-Changelogs-':
-    st.write('Writer 0.2')
+    st.write('`Addon Manager | 0.2`')
     st.success('Added Changelogs')
