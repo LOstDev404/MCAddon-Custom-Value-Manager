115e4086fcbfdae85fe4729a283ffe306e21098d
LOstDev404 <127157866+LOstDev404@users.noreply.github.com>
2024-11-16 17:44:53 +0000

    0.15 (Read Changelogs)

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import zipfile
 import requests
 import streamlit as st
 import uuid
+import time
 import json
 import shutil
 st.set_page_config(
@@ -10,16 +11,18 @@ st.set_page_config(
     page_icon="mcaddon-logo.ico"
     
 )
-def make_mcaddon(mob_eggs, copper, potions, arrows, enchantment_books, source_directory, delay, is_void_gen, output_file, customized):
-    backup_randompick_path = 'Packs/LOstDev404/RandomItemSkyblock/functions/randompick.mcfunction_backup'
-    backup_randomize_path = 'Packs/LOstDev404/RandomItemSkyblock/functions/randomize.mcfunction_backup'
+def make_mcaddon(mob_eggs, copper, potions, arrows, enchantment_books, source_dir, delay, is_void_gen, output_file, customized):
+    backup_path = 'Packs/LOstDev404/RandomItemSkyblock'
 
-    shutil.copyfile('Packs/LOstDev404/RandomItemSkyblock/functions/randompick.mcfunction', backup_randompick_path)
-    shutil.copyfile('Packs/LOstDev404/RandomItemSkyblock/functions/randomize.mcfunction', backup_randomize_path)
+    shutil.copytree('Packs/LOstDev404/RandomItemSkyblock', 'Packs/LOstDev404/RandomItemSkyblock_backup')
+    
     randompick_path = 'Packs/LOstDev404/RandomItemSkyblock/functions/randompick.mcfunction'
     with open(randompick_path, 'r') as f:
         randompick_data = f.read()
-        current_number = 1103
+        current_number = 1
+    while 'itemnumber' in randompick_data:
+        randompick_data = randompick_data.replace('itemnumber', str(current_number), 1) 
+        current_number += 1
     for _ in range(mob_eggs):
         randompick_data += f"\nexecute as @s[scores={{random={current_number}}}] run function variants/runeggs"
         current_number += 1
@@ -40,48 +43,21 @@ def make_mcaddon(mob_eggs, copper, potions, arrows, enchantment_books, source_di
     randomize_path = 'Packs/LOstDev404/RandomItemSkyblock/functions/randomize.mcfunction'
     with open(randomize_path, 'r') as f:
         randomize_data = f.read()
+        current_number -= 1
     randomize_data = randomize_data.replace('itemamount', str(current_number))
     with open(randomize_path, 'w') as f:
         f.write(randomize_data)
-    generate_mcaddon(source_directory, delay, is_void_gen, output_file, customized)
+        
 
-    shutil.copyfile(backup_randompick_path, 'Packs/LOstDev404/RandomItemSkyblock/functions/randompick.mcfunction')
-    shutil.copyfile(backup_randomize_path, 'Packs/LOstDev404/RandomItemSkyblock/functions/randomize.mcfunction')
-    os.remove(backup_randompick_path)
-    os.remove(backup_randomize_path)
-
-def generate_mcaddon(source_directory, delay, is_void_gen, output_file, customized):
-    original_manifest_data, original_tick_data, original_timer_data = modify_files_with_delay(source_directory, delay, is_void_gen)
-
-    dimensions_path = os.path.join(source_directory, 'dimensions')
-    if is_void_gen and os.path.exists(dimensions_path):
-        shutil.move(dimensions_path, dimensions_path + '_disabled')
-    elif not is_void_gen and os.path.exists(dimensions_path + '_disabled'):
-        shutil.move(dimensions_path + '_disabled', dimensions_path)
-
-    zip_files_to_mcaddon(source_directory, output_file)
-    download_link = upload_to_fileio(output_file)
-    st.success(f'Download link: {download_link}')
-
-    revert_files(source_directory, original_manifest_data, original_tick_data, original_timer_data)
-
-    if os.path.exists(dimensions_path + '_disabled'):
-        shutil.move(dimensions_path + '_disabled', dimensions_path)
-
-    os.remove(output_file)
-def generate_uuids():
-    return str(uuid.uuid4()), str(uuid.uuid4())
-
-def modify_files_with_delay(source_dir, delay, is_void_gen):
     manifest_path = os.path.join(source_dir, 'manifest.json')
     tick_path = os.path.join(source_dir, 'functions', 'tick.json')
     timer_path = os.path.join(source_dir, 'functions', 'timer.mcfunction')
-    
+
     uuid1, uuid2 = generate_uuids()
     with open(manifest_path, 'r') as file:
         manifest_data = file.read()
     original_manifest_data = manifest_data
-    
+
     if is_void_gen:
         if customized:
             modified_manifest_data = manifest_data.replace(
@@ -110,7 +86,7 @@ def modify_files_with_delay(source_dir, delay, is_void_gen):
             'packdescription', '§l§cDO NOT PUT ON PRE-EXISTING WORLDS! §f| §l§bInstructions: §r§fPut this on a §l§6new world §r§fduring world creation. §f| §l§bPack created by: §r§aLOde404 / Grexzn'
         )
         start_replacement = 'randomstart'
-    
+
     modified_manifest_data = modified_manifest_data.replace('uuid1', uuid1).replace('uuid2', uuid2).replace('timedelay', str(delay))
 
     with open(tick_path, 'r') as file:
@@ -129,19 +105,32 @@ def modify_files_with_delay(source_dir, delay, is_void_gen):
     with open(timer_path, 'w') as file:
         file.write(modified_timer_data)
 
-    return original_manifest_data, original_tick_data, timer_data
+    dimensions_path = os.path.join(source_dir, 'dimensions')
+    if is_void_gen:
+        if os.path.exists(dimensions_path):
+            if os.path.isdir(dimensions_path):
+                shutil.rmtree(dimensions_path)
+            else:
+                os.remove(dimensions_path)
 
-def revert_files(source_dir, original_manifest_data, original_tick_data, original_timer_data):
-    manifest_path = os.path.join(source_dir, 'manifest.json')
-    tick_path = os.path.join(source_dir, 'functions', 'tick.json')
-    timer_path = os.path.join(source_dir, 'functions', 'timer.mcfunction')
+    zip_files_to_mcaddon(source_dir
+                         , output_file)
+    download_link = upload_to_fileio(output_file)
+    st.success(f'Download link: {download_link}')
+    def deletebackup(backup_path):
+        if os.path.exists(backup_path):
+            if os.path.isdir(backup_path):
+                shutil.rmtree(backup_path)
+            else:
+                os.remove(backup_path)
+    deletebackup(backup_path)
+    shutil.copytree('Packs/LOstDev404/RandomItemSkyblock_backup', 'Packs/LOstDev404/RandomItemSkyblock')
 
-    with open(manifest_path, 'w') as file:
-        file.write(original_manifest_data)
-    with open(tick_path, 'w') as file:
-        file.write(original_tick_data)
-    with open(timer_path, 'w') as file:
-        file.write(original_timer_data)
+    os.remove(output_file)
+    backup_path = 'Packs/LOstDev404/RandomItemSkyblock_backup'
+    deletebackup(backup_path)
+def generate_uuids():
+    return str(uuid.uuid4()), str(uuid.uuid4())
 
 def zip_files_to_mcaddon(source_dir, output_filename):
     with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
@@ -158,7 +147,7 @@ def upload_to_fileio(file_path):
     return response_data.get('link')
 #---------------------------------------- UI Starts Here ----------------------------------------
 
-st.title('MCADDON Custom Value Manager `Version: 0.14`')
+st.title('MCADDON Custom Value Manager `Version: 0.15`')
 st.write('Contact `LOstDev404` on Discord for any bugs, questions, or suggestions.')
 
 main_option = st.selectbox('Choose a pack / option:', ['Random Item Skyblock', '-Changelogs-'])
@@ -177,7 +166,7 @@ if main_option == 'Random Item Skyblock':
             enchantment_books = st.number_input('How much `enchantment book` receive chance do you want?:', min_value=1, step=1, value=2)
             
         if st.button('Get download link'):
-            source_directory = 'Packs/LOstDev404/RandomItemSkyblock'
+            source_dir = 'Packs/LOstDev404/RandomItemSkyblock'
             if ris_option == 'Normal':
                 output_file = f'Random Item Skyblock {delay} Seconds.mcaddon'
                 is_void_gen = False
@@ -191,10 +180,13 @@ if main_option == 'Random Item Skyblock':
                 arrows = 4
                 enchantment_books = 2
 
-            make_mcaddon(mob_eggs, copper, potions, arrows, enchantment_books, source_directory, delay, is_void_gen, output_file, customized)
+            make_mcaddon(mob_eggs, copper, potions, arrows, enchantment_books, source_dir
+                         , delay, is_void_gen, output_file, customized)
             
 if main_option == '-Changelogs-':
-    
+    st.markdown("## **`Addon Manager | 0.15`:**")
+    st.markdown("- Overall optomizations in preparation for Minecraft 1.22\n - Added all types of Suspicious Stew\n - Date: *11/16/2024*")
+    st.write("---")
     st.markdown("## **`Addon Manager | 0.14`:**")
     st.markdown("- Patched a bug causing no 'variants' to be added if the user didn't check the 'Customized futher' checkmark.\n - Date: *10/24/2024*")
     st.write("---")
